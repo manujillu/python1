6eccaae072bc981d2f4189a6e69ca38aae2c9de6
manujillu <50619892+manujillu@users.noreply.github.com>
2019-07-19 09:43:39 +0530

    Create pro14.py

---
+++ pro14.py
@@ -0,0 +1,11 @@
+m11,n1=map(str,input().split())
+t=0
+if l
+for r in raen(m11)>len(n1):
+  m11,n1=n1,m11
+r=0
+while r<len(m11):
+  t+=(ord(n1[r])-ord(m11[r]))
+  r+=1nge(r,len(n1)):
+  t+=ord(n1[r])-ord('a')+1
+print(t)
